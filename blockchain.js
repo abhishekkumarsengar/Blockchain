d220adb8136cd5addaeeac972d7269daf88bdf8b
Abhishek Sengar <abhishekksengar@gmail.com>
2018-04-17 19:00:45 +0530

    Adjusting difficulty based on time taken

--- blockchain.js
+++ blockchain.js
@@ -1,4 +1,6 @@
 const Block = require('./block');
+const BLOCK_GENERATION_INTERVAL = 6000;
+const DIFFICULTY_ADJUSTMENT_INTERVAL = 2016;
 
 class Blockchain {
 
@@ -29,6 +31,7 @@ class Blockchain {
      */
     addNewBlock(newBlock) {
         newBlock.mineBlock(this.difficulty);
+        this.isChainValid();
         newBlock.index = this.fetchLatestBlock().index + 1;
         newBlock.previousHash = this.fetchLatestBlock().hash;
         newBlock.hash = newBlock.calculateHash();
@@ -63,7 +66,32 @@ class Blockchain {
         return true;
     }
 
+    getDifficulty (blockchain) {
+        console.log(blockchain.chain.length);
+        const latestBlock = this.fetchLatestBlock();
+        if (latestBlock.index % DIFFICULTY_ADJUSTMENT_INTERVAL === 0 && latestBlock.index !== 0) {
+            return this.getAdjustedDifficulty(latestBlock, this.chain);
+        } else {
+            return blockchain.difficulty;
+        }
+    };
+
+    getAdjustedDifficulty(latestBlock, blockchain) {
+        const prevAdjustmentBlock = blockchain[blockchain.length - DIFFICULTY_ADJUSTMENT_INTERVAL];
+        const timeExpected = BLOCK_GENERATION_INTERVAL * DIFFICULTY_ADJUSTMENT_INTERVAL;
+        const timeTaken = latestBlock.timestamp - prevAdjustmentBlock.timestamp;
+
+        if (timeTaken < timeExpected/2) {
+            return this.difficulty + 1;
+        } else if (timeTaken > timeExpected*2) {
+            return this.difficulty - 1;
+        } else {
+            return this.difficulty;
+        }
+    }
+
 }
+
 
 /**
  * Creating instance of the blockchain and adding new blocks to the
@@ -71,7 +99,13 @@ class Blockchain {
  * @type {Blockchain}
  */
 let chainCoin = new Blockchain();
-chainCoin.addNewBlock(new Block("{amount : 4}"));
-chainCoin.addNewBlock(new Block("{amount : 10}"));
+
+for (let i=0 ; i<DIFFICULTY_ADJUSTMENT_INTERVAL; i++) {
+    console.log("Block " + i);
+    chainCoin.addNewBlock(new Block("{amount : "+ i + "}"));
+}
+
+const getDifficulty = chainCoin.getDifficulty(chainCoin);
+chainCoin.difficulty = getDifficulty;
 
 console.log(chainCoin);
